fc4db28b37d1aa6343cf0b8588eb8b5f6fe249b1
XxMoonsoulxX <77117705+XxMoonsoulxX@users.noreply.github.com>
2022-01-19 15:05:20 +0200

    level3 resetbutton
    teksti keltaiselle

---
+++ TEVAProject/Assets/Scripts/Level3 Scripts/ResetButton3.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetButton3 : MonoBehaviour
+{
+    public Transform target;
+    public void resetSquare1()
+    {
+        transform.position = target.position;
+    }
+
+    public void resetSquare2()
+    {
+        transform.position = target.position;
+    }
+
+    public void resetSquare3()
+    {
+        transform.position = target.position;
+    }
+
+    public void resetSquare4()
+    {
+        transform.position = target.position;
+    }
+
+    public void resetSquare5()
+    {
+        transform.position = target.position;
+    }
+
+    public void resetSquare6()
+    {
+        transform.position = target.position;
+    }
+
+    public void resetSquare7()
+    {
+        transform.position = target.position;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
